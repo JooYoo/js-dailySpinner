5a5c69fa68c09712f60099669fc6c2f43a2c799c
jooyoo <jooyoo@outlook.com>
2020-09-05 16:34:32 +0200

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -40,7 +40,7 @@ async function registerSW() {
   if ('serviceWorker' in navigator) {
     if ('serviceWorker' in navigator) {
       try {
-        await navigator.serviceWorker.register('../../sw.js');
+        await navigator.serviceWorker.register('./sw.js');
       } catch (e) {
         console.log(`SW registration failed`);
       }
