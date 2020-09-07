89d0aaef795b62caf609a712c800636a41ee7100
jooyoo <jooyoo@outlook.com>
2020-09-07 21:11:28 +0200

    shortcut toast

--- src/components/yu-toast.js
+++ src/components/yu-toast.js
@@ -5,17 +5,29 @@ template.innerHTML = `
        position: absolute;
        transform: translateX(-50%);
        left: 50%;
+       display: flex;
+       justify-content: space-around;
+       width: 100%;
        bottom: 3%;
+    }
+
+    .toast__item {
        padding: 1% 10%;
        border-radius: 10px;
        box-shadow: -6px -6px 16px rgba(255, 255, 255, 1), 6px 6px 16px rgba(209, 205, 199, 1);
        font-size: 3vmin;
        color: rgba(0, 0, 0, 0.7);
-
+       white-space: nowrap;
     }
+
 </style>
 
-<div class="toast-container">hello, I'm Component Toast</div>
+<div class="toast-container">
+  <span class="toast__item">Enter: spin</span>
+  <span class="toast__item">F: flip</span>
+  <span class="toast__item">R: reset</span>
+</div>
+
 `;
 
 class YuToast extends HTMLElement {
