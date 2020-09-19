3b8570f47077418274af58ea135b5dc5a5f06fb6
jooyoo <jooyoo@outlook.com>
2020-09-19 15:55:15 +0200

    progressRing percent

--- src/js/ui_progress-ring.js
+++ src/js/ui_progress-ring.js
@@ -22,10 +22,48 @@ function setProgress(restPeople, allPeople) {
 }
 
 function setProgressUi(progressPercent) {
+  // set spin progressbar
   document.documentElement.style.setProperty(
     '--spin-progressbar-percent',
     progressPercent
   );
+
+  // set progressbar percent
+  setPercent(progressPercent);
+}
+
+function setPercent(targetNummber) {
+  // get progressPercentEl
+  let progressPercentEl = document.querySelector('#spin-progressbar__percent');
+
+  // get currentNum
+  let currentNum = +progressPercentEl.innerHTML.slice(0, -1);
+  if (!currentNum) currentNum = 0;
+
+  // round the targetNum
+  let targetNum = Math.round(targetNummber);
+
+  // count up && down animation
+  let displayNum = currentNum;
+  countAnim(progressPercentEl, displayNum, targetNum);
+}
+
+function countAnim(progressPercentEl, displayNum, targetNum) {
+  if (targetNum != 0) {
+    const countUpAnim = () => {
+      displayNum++;
+      if (displayNum != targetNum) setTimeout(countUpAnim, 40);
+      progressPercentEl.innerHTML = `${displayNum}%`;
+    };
+    countUpAnim();
+  } else if (displayNum != 0) {
+    const countDownAnim = () => {
+      displayNum--;
+      if (displayNum != 0) setTimeout(countDownAnim, 30);
+      progressPercentEl.innerHTML = `${displayNum}%`;
+    };
+    countDownAnim();
+  }
 }
 
 export { setProgressUi, setProgress };
