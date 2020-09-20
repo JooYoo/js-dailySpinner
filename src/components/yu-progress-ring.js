9ef76cdf8ae2c16f82291e08763006fb2349435f
jooyoo <jooyoo@outlook.com>
2020-09-20 07:50:46 +0200

    pass bg color as web component attribute

--- src/components/yu-progress-ring.js
+++ src/components/yu-progress-ring.js
@@ -1,3 +1,5 @@
+import * as uiUtility from '../js/ui_utility.js';
+
 const template = document.createElement('template');
 template.innerHTML = `
 <style>
@@ -25,7 +27,6 @@ template.innerHTML = `
     fill: none;
     stroke-width: 8px;
     stroke-dasharray: 440;
-    // stroke-dashoffset: 1240;
     stroke-linecap: round;
     overflow: visible;
 }
@@ -56,8 +57,8 @@ template.innerHTML = `
 
 <div class="spin-progressbar">
     <svg viewbox="0 0 140 140">
-      <circle cx="70" cy="70" r="60"></circle>
-      <circle cx="70" cy="70" r="60"></circle>
+      <circle id="ring-bg" cx="70" cy="70" r="60"></circle>
+      <circle id="ring" cx="70" cy="70" r="60"></circle>
     </svg>
 </div>
 <div id="spin-progressbar__percent" class="spin-progressbar__percent">
@@ -70,6 +71,15 @@ class YuProgressRing extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    // get high-light-color
+    let ringEl = this.shadowRoot.querySelector('#ring');
+    let ringColor = this.getAttribute('high-light-color');
+    // set ringEl high-light-color
+    ringEl.style.stroke = ringColor;
+    // TODO: set ringBgEl color
+
+    console.log(ringColor);
   }
 }
 
