58a312a226e4136e728793bf4ef27f043c194072
jooyoo <jooyoo@outlook.com>
2020-10-28 17:08:27 +0100

    Updates

--- src/components/yu-progress-ring.js
+++ src/components/yu-progress-ring.js
@@ -4,10 +4,8 @@ const template = document.createElement('template');
 template.innerHTML = `
 <style>
 :host{
-  --circle-r: 60;
   --circle-bg-dashoffset: 120;
   --numeric-height: 18%;
-  --progress-value: 0;
 
   position: absolute;
   height: 100%;
@@ -30,24 +28,14 @@ template.innerHTML = `
 }
 
 .spin-progressbar circle:nth-child(1) {
-    cx: 70;
-    cy: 70;
-    r: var(--circle-r);
-
     stroke: rgba(0, 0, 0, 0.02);
     stroke-dashoffset: var(--circle-bg-dashoffset);
 }
 
 .spin-progressbar circle:nth-child(2) {
-    cx: 70;
-    cy: 70;
-    r: var(--circle-r);
-
     stroke-width: 7px;
     stroke: rgba(0, 0, 0, 0.15);
-    stroke-dashoffset: calc(
-      440 - ((440 - var(--circle-bg-dashoffset)) * var(--progress-value)) / 100
-    );
+    stroke-dashoffset: 440;
     
     transition: stroke-dashoffset 1s ease-out;
 }
@@ -72,8 +60,8 @@ template.innerHTML = `
 
 <div class="spin-progressbar">
     <svg viewbox="0 0 140 140">
-      <circle id="ring-bg"></circle>
-      <circle id="ring"></circle>
+      <circle id="ring-bg" cx="70" cy="70"></circle>
+      <circle id="ring" cx="70" cy="70"></circle>
     </svg>
 </div>
 <div id="numeric-text" class="numeric-text">
@@ -97,14 +85,12 @@ class YuProgressRing extends HTMLElement {
     // set circleR, circleDashOffset to CSS var
     uiUtility.setCssVarShadowRoot(
       this.shadowRoot.host,
-      '--circle-r',
-      circleRAtt
-    );
-    uiUtility.setCssVarShadowRoot(
-      this.shadowRoot.host,
       '--circle-bg-dashoffset',
-      circleDashOffsetAtt
+      circleDashOffsetAtt,
     );
+
+    this.shadowRoot.querySelector('#ring-bg').setAttribute('r', circleRAtt);
+    this.shadowRoot.querySelector('#ring').setAttribute('r', circleRAtt);
 
     /* -------------------------- set numeric position -------------------------- */
     // get numeric-height from DOM
@@ -113,7 +99,7 @@ class YuProgressRing extends HTMLElement {
     uiUtility.setCssVarShadowRoot(
       this.shadowRoot.host,
       '--numeric-height',
-      numericAtt
+      numericAtt,
     );
 
     /* ----------------------------- set ring color ----------------------------- */
@@ -139,20 +125,20 @@ class YuProgressRing extends HTMLElement {
   getRingColor(thisComponentId) {
     let thisColor;
     switch (thisComponentId) {
-      case "mainTimer":
+      case 'mainTimer':
         thisColor = uiUtility.getCssVar('--color-ring-main-timer');
         break;
-      
-      case "progressRing":
+
+      case 'progressRing':
         thisColor = uiUtility.getCssVar('--color-ring-individual-timer');
         break;
 
-      case "personTimer":
+      case 'personTimer':
         thisColor = uiUtility.getCssVar('--color-ring-progress');
         break;
-      
-        default:
-          break;
+
+      default:
+        break;
     }
 
     return thisColor;
